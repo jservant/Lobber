a4891bdd142dab1a47c6b2b83d41d7c8c672c80c
Jaden Servant <48299534+jservant@users.noreply.github.com>
2023-02-06 14:00:58 -0500

    Lob work + Resources folder reorg

--- Assets/Scripts/Jaden/PlayerController.cs
+++ Assets/Scripts/Jaden/PlayerController.cs
@@ -6,69 +6,71 @@ using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
-    CapsuleCollider capCol;
+    //CapsuleCollider capCol;
     Rigidbody rb;
     Animator animr;
     bool animBuffer = false;
     MeshRenderer headMesh;
+    public GameObject headProj;
+    Transform projSpawn;
 
     Vector2 mInput;
-    [SerializeField] float pSpeed = 2f;
+    [SerializeField] float speed = 2f;
+    [SerializeField] float projSpeed = 2f;
     [SerializeField] int damage = 5;
 
-    private void Awake()
-    {
-        capCol = GetComponent<CapsuleCollider>();
+    private void Awake() {
+        //capCol = GetComponent<CapsuleCollider>();
         rb = GetComponent<Rigidbody>();
         animr = GetComponent<Animator>();
         headMesh = transform.Find("Axe_Controller/AxeHitbox/Sphere").GetComponent<MeshRenderer>();
+        projSpawn = transform.Find("ProjSpawn");
         #region debug
         if (headMesh != null) { Debug.Log("Axe headmesh found on player."); }
         else { Debug.LogWarning("Axe headmesh not found on player."); }
         #endregion
     }
 
-    private void Update()
-    {
+    private void Update() {
         Vector3 movement = new Vector3(mInput.x, 0, mInput.y).normalized;
-        rb.MovePosition(transform.position + movement * Time.deltaTime * pSpeed);
+        rb.MovePosition(transform.position + movement * Time.deltaTime * speed);
 
     }
 
     #region Player inputs
-    public void Move(InputAction.CallbackContext context)
-    {
+    public void Move(InputAction.CallbackContext context) {
         mInput = context.ReadValue<Vector2>();
         if (context.performed == true) { transform.rotation = Quaternion.LookRotation(new Vector3(mInput.x, 0, mInput.y)); }
         //Debug.Log("Move activated, current value: " + mInput);
     }
 
-    public void Attack(InputAction.CallbackContext context)
-    {
+    public void Attack(InputAction.CallbackContext context) {
         if (animBuffer == false) StartCoroutine(AnimBuffer("attack", .73f, true));
     }
 
-    public void Lob(InputAction.CallbackContext context)
-    {
+    public void Lob(InputAction.CallbackContext context) {
         if (animBuffer == false) {
             if (headMesh.enabled == true) {
                 StartCoroutine(AnimBuffer("lobThrow", .7f, true));
-                headMesh.enabled = false;
-                //TODO(@Jaden): Create a projectile
+                // all functionality following is in LobThrow which'll be triggered in the animator
             }
             else StartCoroutine(AnimBuffer("lob", .73f, true));
         }
     }
 
-    public void Restart(InputAction.CallbackContext context)
-    {
+    public void Restart(InputAction.CallbackContext context) {
         Debug.Log("Restart called");
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
     #endregion
 
-    private void OnTriggerEnter(Collider other) // trigger SHOULD be axe hitbox
-    {
+    public void LobThrow() { // triggered in animator
+        headMesh.enabled = false;
+        GameObject iHeadProj = Instantiate(headProj, projSpawn.position, new Quaternion(0,0,0,1)); // FIX THIS
+        iHeadProj.transform.Translate(new Vector3(mInput.x, 0, mInput.y) * projSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other) { // trigger SHOULD be axe hitbox
         if (other.gameObject.layer == 6 && animr.GetBool("lob") == false) {//Enemy
             other.gameObject.GetComponent<Enemy>().ReceiveDamage(damage);
         }
@@ -83,8 +85,7 @@ public class PlayerController : MonoBehaviour
     // so it doesn't melt their health
 
     #region Minor utility functions
-    IEnumerator AnimBuffer(string animName, float duration, bool offWhenDone)
-    {
+    IEnumerator AnimBuffer(string animName, float duration, bool offWhenDone) {
         animr.SetBool(animName, true);
         animBuffer = true;
         yield return new WaitForSeconds(duration);
