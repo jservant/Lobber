be45e94e6f648cfafe9e1a51e61c27aeeb2073e4
perplexedMurfy <perplexedMurfy@gmail.com>
2023-02-07 16:43:43 -0500

    Add layer mask generation

--- Assets/Scripts/Roskuski/Layers.cs
+++ Assets/Scripts/Roskuski/Layers.cs
@@ -12,3 +12,16 @@ public enum Layers : int {
     PlayerHitbox,
     PlayerHurtbox,
 }
+
+public class Mask {
+    public static int Get(Layers[] array) {
+        int Result = 0;
+        foreach (Layers layer in array) {
+            Result |= 1<<((int)layer);
+        }
+        return Result; 
+    }
+    public static int Get(Layers layer) {
+        return Get(new Layers[]{layer});
+    }
+}
