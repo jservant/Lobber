b1e79d1d5965a6240ba7ffbc0f609abb006c9cdc
Rerric <79161388+Rerric@users.noreply.github.com>
2023-03-16 15:01:16 -0400

    Implemented Prototype Head Popup

---
+++ Assets/Objects/Player/HeadPopped.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadPopped : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float lifetime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (lifetime >= 0)
+        {
+            lifetime -= Time.deltaTime;
+        }
+        else DestroySkull();
+
+        if (transform.position.y <= 0)
+        {
+            DestroySkull();
+        }
+    }
+
+    void DestroySkull()
+    {
+        Destroy(this.transform.parent.gameObject);
+    }
+}
