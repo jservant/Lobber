6e178bf8d6d14de2b5125a2a7180ac68bcd21a17
Jaden Servant <48299534+jservant@users.noreply.github.com>
2023-03-20 12:06:35 -0400

    Orb spawner works (not very well)

--- Assets/Objects/OrbSpawn.cs
+++ Assets/Objects/OrbSpawn.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class OrbSpawn : MonoBehaviour
 {
-
     public bool spawnNow;
     public GameObject orbPrefab;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
 
     void Update()
     {
