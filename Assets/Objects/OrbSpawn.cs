0ae8ad28e4ec969d6c0567f5148d91d5389ff0cf
Jaden Servant <48299534+jservant@users.noreply.github.com>
2023-03-21 14:54:58 -0400

    Enemy spawns actually really fixed this time

--- Assets/Objects/OrbSpawn.cs
+++ Assets/Objects/OrbSpawn.cs
@@ -2,19 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class OrbSpawn : MonoBehaviour
-{
+public class OrbSpawn : MonoBehaviour {
+    public float despawnTime;
+    public GameObject enemy;
+    public Animator anim;
+    public Transform[] spawns;
     public bool spawnNow;
-    public GameObject orbPrefab;
+    GameManager gameMan;
+    [SerializeField] GameObject orbPrefab;
 
-    void Update()
-    {
-        if (spawnNow == true)
-        {
+    void Start() {
+        spawns = transform.Find("Spawns").GetComponentsInChildren<Transform>();
+        //orbPrefab = transform.Find("Orb");
+        anim = orbPrefab.GetComponent<Animator>();
+        gameMan = transform.Find("/GameManager").GetComponent<GameManager>();
+    }
+
+    void Update() {
+        if (spawnNow == true) {
             spawnNow = false;
-            Instantiate (orbPrefab, transform);
+            StartCoroutine(Spawning(despawnTime));
         }
+    }
 
+    IEnumerator Spawning(float despawnTime) {
+        orbPrefab.SetActive(true);
+        if (gameMan.canSpawn == true) gameMan.canSpawn = false;
+        yield return new WaitForSeconds(despawnTime / 2);
+        for (int s = 0; s < spawns.Length; s++) {
+            Instantiate(enemy, spawns[s].transform.position, spawns[s].transform.rotation);
+        }
+        yield return new WaitForSeconds(despawnTime / 2);
+        anim.SetBool("DeSpawn", true);
+        yield return new WaitForSeconds(0.5f);
+        orbPrefab.SetActive(false);
+        gameMan.canSpawn = true;
     }
 
 }
