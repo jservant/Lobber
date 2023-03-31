1a31f3634028f5ba63d20c536bb4f12942969bce
Jaden Servant <48299534+jservant@users.noreply.github.com>
2023-03-31 13:53:24 -0400

    Bone pile adjustments

--- Assets/Objects/Decorations/BonePile.cs
+++ Assets/Objects/Decorations/BonePile.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class BonePile : MonoBehaviour
 {
 	PlayerController player;
+	public GameObject headPop;
 
 	private void Start() {
 		player = transform.Find("/Player").GetComponent<PlayerController>();
@@ -12,7 +13,10 @@ public class BonePile : MonoBehaviour
 
 	private void OnTriggerEnter(Collider other) {
 		if (other.gameObject.layer == (int)Layers.PlayerHitbox) { // player is hitting enemy
-			player.ChangeMeter(1);
+			//player.ChangeMeter(1);
+			GameObject headInstance = Instantiate(headPop, transform.position, transform.rotation);
+			HeadPickup hpop = headInstance.transform.Find("Head").GetComponent<HeadPickup>();
+			hpop.randomForce = 5f;
 			Destroy(gameObject);
 		}
 	}
