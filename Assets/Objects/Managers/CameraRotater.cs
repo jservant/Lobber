083a9504a75cb553a47eabea07de27eaf6188cc9
Rerric <79161388+Rerric@users.noreply.github.com>
2023-04-03 17:12:07 -0400

    Fixed Camera Control Script

--- Assets/Objects/Managers/CameraRotater.cs
+++ Assets/Objects/Managers/CameraRotater.cs
@@ -8,6 +8,7 @@ public class CameraRotater : MonoBehaviour {
 	float yConst;
 	PlayerController player;
 	Vector2 cInput;
+	public GameObject camera2;
 
 	void Start() {
 		player = transform.Find("/GameManager").GetComponent<GameManager>().playerController;
@@ -19,6 +20,7 @@ public class CameraRotater : MonoBehaviour {
 	void FixedUpdate() {
 		cInput = player.pActions.Player.Aim.ReadValue<Vector2>();
 		transform.position = new Vector3(player.transform.position.x, yConst, player.transform.position.z);
-		transform.Rotate(cInput.y * cSpeed * Time.fixedDeltaTime, cInput.x * cSpeed * Time.fixedDeltaTime, 0f, Space.World);
+		transform.Rotate(0f, cInput.x * cSpeed * Time.fixedDeltaTime, 0f, Space.World);
+		camera2.transform.Rotate(cInput.y * -cSpeed * Time.fixedDeltaTime, 0f, 0f, Space.Self);
 	}
 }
