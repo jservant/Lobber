3a797f160d2c2ea0dc1b144e49a38ae51e54321c
perplexedMurfy <perplexedMurfy@gmail.com>
2023-04-05 11:44:04 -0400

    Remove dead code

--- Assets/Objects/Player/HeadProjectile.cs
+++ Assets/Objects/Player/HeadProjectile.cs
@@ -23,7 +23,6 @@ public class HeadProjectile : MonoBehaviour {
 	void Update() {
 		transform.Translate(Vector3.forward * speed * Time.deltaTime);
 		head.Rotate(Vector3.forward, 1000 * Time.deltaTime);
-		//head.eulerAngles = new Vector3(0, 5, 0);
 	}
 
 	private void OnTriggerEnter(Collider other) {
@@ -41,12 +40,6 @@ public class HeadProjectile : MonoBehaviour {
 		}
 	}
 
-	/*private void OnCollisionEnter(Collision collision) {
-		if (collision.gameObject.layer == (int)Layers.EnemyHurtbox) {
-			Debug.Log("proj should die lol");
-			Destroy(gameObject);
-		}
-	}*/
 	void OnDrawGizmos() {
 		Gizmos.color = Color.white;
 		Gizmos.DrawWireSphere(transform.position, stunSphereRadius);
