06e53b7370e46773af89b6ab5c3fcc3c28658a43
Rerric <79161388+Rerric@users.noreply.github.com>
2023-04-11 12:04:14 -0400

    Hitflash on Bonepile progress

--- Assets/Objects/Decorations/BonePile.cs
+++ Assets/Objects/Decorations/BonePile.cs
@@ -12,16 +12,36 @@ public class BonePile : MonoBehaviour
 	public int headTotal; //about how many heads the pile can contain
 	int heads = 0;
 
+	float hitflashTimer = 0f;
+	MeshRenderer model;
+	Material[] materials;
+	public Material hitflashMat;
+
 	private void Start() {
 		player = transform.Find("/Player").GetComponent<PlayerController>();
 		gameMan = transform.Find("/GameManager").GetComponent<GameManager>();
 		heads = Random.Range(headTotal - 3, headTotal + 3);
 	}
 
-	private void OnTriggerEnter(Collider other) {
+    void Update() {
+		hitflashTimer -= Time.deltaTime;
+		Material[] materialList = model.materials;
+		for (int i = 0; i < materialList.Length; i++) {
+			if (hitflashTimer > 0) {
+				materialList[i] = hitflashMat;
+			}
+			else {
+				materialList[i] = materials[i];
+			}
+		}
+		model.materials = materialList;
+	}
+
+    private void OnTriggerEnter(Collider other) {
 		if (other.gameObject.layer == (int)Layers.PlayerHitbox) { // player is hitting enemy
 			Vector3 spawnPoint = new Vector3(transform.position.x, transform.position.y + 1f, transform.position.z);
 			gameMan.SpawnParticle(0, spawnPoint, 2f);
+			hitflashTimer = 0.25f;
 			int random = Random.Range(2, 3);
 			SpawnHeads(random);
 			if (heads <= 0) Destroy(gameObject);
