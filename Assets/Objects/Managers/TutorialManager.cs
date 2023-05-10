5e5636e15775be205e98470ba9683256f0966876
perplexedMurfy <perplexedMurfy@gmail.com>
2023-05-10 16:31:13 -0400

    Formatting pass on TutorialManager.cs

--- Assets/Objects/Managers/TutorialManager.cs
+++ Assets/Objects/Managers/TutorialManager.cs
@@ -3,75 +3,73 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class TutorialManager : MonoBehaviour
-{
-    GameManager gameMan;
-    private Transform player;
-    public GameObject[] areas;
-    public Transform[] playerRespawnPoints;
-    public List<GameObject> currentTargets = new List<GameObject>();
-    public int areasCompleted = 0;
-    public bool targetsExist;
-    public bool skipTutorial;
+public class TutorialManager : MonoBehaviour {
+	GameManager gameMan;
+	private Transform player;
+	public GameObject[] areas;
+	public Transform[] playerRespawnPoints;
+	public List<GameObject> currentTargets = new List<GameObject>();
+	public int areasCompleted = 0;
+	public bool targetsExist;
+	public bool skipTutorial;
 
-    private void Awake() {
-        gameMan = transform.Find("/GameManager").GetComponent<GameManager>();
-        player = transform.Find("/Player");
-        UpdateAreas();
-    }
+	private void Awake() {
+		gameMan = transform.Find("/GameManager").GetComponent<GameManager>();
+		player = transform.Find("/Player");
+		UpdateAreas();
+	}
 
-    void Start()
-    {
-       if (Initializer.save.versionLatest.tutorialComplete == true) { 
-            gameMan.playerController.transform.position = playerRespawnPoints[playerRespawnPoints.Length-1].position;
+	void Start() {
+		if (Initializer.save.versionLatest.tutorialComplete == true) { 
+			gameMan.playerController.transform.position = playerRespawnPoints[playerRespawnPoints.Length-1].position;
 		}
-    }
+	}
 
-    // Update is called once per frame
-    void Update() {
-        targetsExist = CheckTargets();
-        if (!targetsExist && (areasCompleted < 5)) {
-            areasCompleted += 1;
-            UpdateAreas();
-        }
+	// Update is called once per frame
+	void Update() {
+		targetsExist = CheckTargets();
+		if (!targetsExist && (areasCompleted < 5)) {
+			areasCompleted += 1;
+			UpdateAreas();
+		}
 
-        if (player.position.z > 65f) {
-            areasCompleted = 6;
-            Initializer.save.versionLatest.tutorialComplete = true;
-            UpdateSpawns();
-        }
-    }
+		if (player.position.z > 65f) {
+			areasCompleted = 6;
+			Initializer.save.versionLatest.tutorialComplete = true;
+			UpdateSpawns();
+		}
+	}
 
-    void UpdateAreas() {
-        for (int i = 0; i < areas.Length; i++) {
-            if (i <= areasCompleted) {
-                areas[i].SetActive(true);
-            }
-            else areas[i].SetActive(false);
-        }
-        currentTargets.Clear();
+	void UpdateAreas() {
+		for (int i = 0; i < areas.Length; i++) {
+			if (i <= areasCompleted) {
+				areas[i].SetActive(true);
+			}
+			else areas[i].SetActive(false);
+		}
+		currentTargets.Clear();
 
-        foreach (Transform child in areas[areasCompleted].transform) {
-            if (child.gameObject.layer == (int)Layers.EnemyHurtbox) currentTargets.Add(child.gameObject);
-        }
+		foreach (Transform child in areas[areasCompleted].transform) {
+			if (child.gameObject.layer == (int)Layers.EnemyHurtbox) currentTargets.Add(child.gameObject);
+		}
 
-        UpdateSpawns();
-    }
+		UpdateSpawns();
+	}
 
 
-    void UpdateSpawns() { 
-        for (int i = 0; i < playerRespawnPoints.Length; i++) {
-            playerRespawnPoints[i].gameObject.SetActive(false);
-        }
-        playerRespawnPoints[areasCompleted].gameObject.SetActive(true);
-        gameMan.UpdatePlayerSpawns();
-    }
+	void UpdateSpawns() { 
+		for (int i = 0; i < playerRespawnPoints.Length; i++) {
+			playerRespawnPoints[i].gameObject.SetActive(false);
+		}
+		playerRespawnPoints[areasCompleted].gameObject.SetActive(true);
+		gameMan.UpdatePlayerSpawns();
+	}
 
-    private bool CheckTargets() {
-        for (int n = 0; n < currentTargets.Count; n++) {
-            if (currentTargets[n] != null) return true;
-        }
-        return false;
-    }
+	private bool CheckTargets() {
+		for (int n = 0; n < currentTargets.Count; n++) {
+			if (currentTargets[n] != null) return true;
+		}
+		return false;
+	}
 
 }
