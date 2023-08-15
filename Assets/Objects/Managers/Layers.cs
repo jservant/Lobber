e0d8dcaa1f40178b7a978c4833d54d5926b00bf9
Jaden S <jbroxnari@protonmail.com>
2023-08-15 14:46:08 -0400

    NO MORE CONECASTALL
    IT'S BAD, NO MORE

--- Assets/Objects/Managers/Layers.cs
+++ Assets/Objects/Managers/Layers.cs
@@ -165,28 +165,4 @@ public class Util {
 
 		return result;
 	}
-
-	// Credit for ConeCast code: Copyright (c) 2018 Walter Ellis
-	// https://github.com/walterellisfun/ConeCast/blob/master/LICENSE
-	public static RaycastHit[] ConeCastAll(Vector3 origin, float maxRadius, Vector3 direction, float maxDistance, float coneAngle) {
-		RaycastHit[] sphereCastHits = Physics.SphereCastAll(origin - new Vector3(0, 0, maxRadius), maxRadius, direction, maxDistance, Mask.Get(Layers.EnemyHurtbox));
-		List<RaycastHit> coneCastHitList = new List<RaycastHit>();
-
-		if (sphereCastHits.Length > 0) {
-			for (int i = 0; i < sphereCastHits.Length; i++) {
-				Vector3 hitPoint = sphereCastHits[i].point;
-				Vector3 directionToHit = hitPoint - origin;
-				float angleToHit = Vector3.Angle(direction, directionToHit);
-
-				if (angleToHit < coneAngle) {
-					coneCastHitList.Add(sphereCastHits[i]);
-				}
-			}
-		}
-
-		RaycastHit[] coneCastHits = new RaycastHit[coneCastHitList.Count];
-		coneCastHits = coneCastHitList.ToArray();
-
-		return coneCastHits;
-	}
 }
